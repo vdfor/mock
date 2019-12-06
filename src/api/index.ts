6afb132596ab2d09319bc9869e357d1e3e887c3f
vdfor <jiakun.li@qq.com>
2019-12-06 21:16:54 +0800

    add china-region api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,4 @@
 export { default as testApi } from './test';
 export { default as userApi } from './user';
+export { default as chinaRegionApi } from './china-region';
 export * from './quark-mobile';
